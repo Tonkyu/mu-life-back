8f562b7e853e0b6587e179d6b53742059bbf040f
Atsuhisa Nakane <tontonkyu@gmail.com>
2023-03-15 20:27:40 +0900

    modify cors

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ const { Configuration, OpenAIApi } = require("openai");
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  // res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  // res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-  // res.header('Access-Control-Max-Age', '86400');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Max-Age', '86400');
   next();
 });
 // jsonデータを扱う
